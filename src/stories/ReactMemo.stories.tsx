8900c380732819baf2639a7c81e75642c79741e6
BeautyVika <vikachk54@gmail.com>
2022-10-31 17:09:04 +0300

    added ReactMemo and UseMemo

---
+++ src/stories/ReactMemo.stories.tsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react';
+
+export default {
+    title: 'React.memo demo'
+
+}
+const NewMessagesCounter = (props: {count: number}) => {
+    return <div>{props.count}</div>
+}
+const UsersSecret = (props: {users: Array<string>}) => {
+    return <div>{props.users.map((u, index)=> <div key={index}>{u}</div>)}</div>
+}
+const Users = React.memo(UsersSecret)
+
+export const Example1 = () => {
+    const [counter, setCounter] = useState(0)
+    const [users, setUsers] = useState(['Dimych', 'Vika', 'Max', 'Andrew'])
+    const adduser = () => {
+        const newUsers = [...users, 'Sveta ' + new Date().getTime()]
+        setUsers(newUsers)
+    }
+    // users.push('Sveta ' + new Date().getTime())
+    return <>
+        <button onClick={() => setCounter((counter + 1))}>+</button>
+        <button onClick={adduser}>add user</button>
+        <NewMessagesCounter count={counter}/>
+        <Users users={users}/>
+    </>
+}
