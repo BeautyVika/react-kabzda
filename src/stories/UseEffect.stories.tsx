76a4f2358ed0a373b28e2bf6db6587d763ca3961
BeautyVika <vikachk54@gmail.com>
2022-11-14 16:47:17 +0300

    added useEffect

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from 'react';
+
+export default {
+    title: 'useEffect demo'
+}
+
+export const SimpleExample = () => {
+    const [counter, setCounter] = useState(1)
+    console.log('Simple example')
+
+    useEffect(() => {
+        console.log('UseEffect')
+        document.title = counter.toString()
+    },[counter])
+
+    return <>
+        Hello,  {counter}
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+    </>
+}
+
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetTimeout Example')
+
+    useEffect(() => {
+        setTimeout(() => {
+            document.title = counter.toString()
+        }, 1000)
+    },[counter])
+
+
+    return <>
+        Hello,  {counter}
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+    </>
+}
+
+export const SetIntervalExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    console.log('SetInterval Example')
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter((state) => state + 1)
+        }, 1000)
+    },[])
+
+
+    return <>
+        Hello,  {counter}
+    </>
+}
+
+export const ClockExample = () => {
+
+    const [clock, setClock] = useState(Date.now().toString())
+
+
+    useEffect(() => {
+        setInterval(() => {
+            setClock(new Date().toLocaleTimeString())
+        }, 1000)
+    },[])
+
+
+    return <>
+        Hello,  {clock}
+    </>
+}
